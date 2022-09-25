f8bb8b87d7cc9dd45161d4a0cee39276b80abb4d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2022-09-24 19:17:26 -0500

    Futz with colors.

--- app/Colors.hs
+++ app/Colors.hs
@@ -9,12 +9,12 @@ import Types
 color :: MidiNote -> Color
 color = maybe "000000" id . flip M.lookup color_map
 
--- PITFALL: These are piano-like for 41-edo.
--- For some other edo, they probably aren't.
+-- PITFALL: These work out nicely for 41-edo.
+-- For some other edo, they might not.
 color_map :: Map MidiNote Color
 color_map = M.fromList [
   -- "naturals"
-  ( 0, "ffffff"),
+  ( 0, "aaaaaa"),
   ( 7, "ff0000"),
   (14, "ff0000"),
   (17, "ff0000"),
@@ -22,20 +22,26 @@ color_map = M.fromList [
   (31, "ff0000"),
   (38, "ff0000"),
 
-  -- "flats"
-  (37, "ffffff"),
-  (3 , "00ff00"),
-  (10, "00ff00"),
-  (13, "00ff00"),
-  (20, "00ff00"),
-  (27, "00ff00"),
-  (34, "00ff00"),
+  ( 1, "aaaaaa"),
+  ( 8, "00ff00"),
+  (15, "00ff00"),
+  (18, "00ff00"),
+  (25, "00ff00"),
+  (32, "00ff00"),
+  (39, "00ff00"),
 
-  -- "sharps"
-  (4 , "ffffff"),
-  (11, "0000ff"),
-  (18, "0000ff"),
-  (21, "0000ff"),
-  (28, "0000ff"),
-  (35, "0000ff"),
-  (1 , "0000ff") ]
+  ( 2, "aaaaaa"),
+  ( 9, "0000ff"),
+  (16, "0000ff"),
+  (19, "0000ff"),
+  (26, "0000ff"),
+  (33, "0000ff"),
+  (40, "0000ff"),
+
+  ( 3, "aaaaaa"),
+  (10, "aaaa00"),
+  (17, "aaaa00"),
+  (20, "aaaa00"),
+  (27, "aaaa00"),
+  (34, "aaaa00"),
+  (41, "aaaa00") ]
